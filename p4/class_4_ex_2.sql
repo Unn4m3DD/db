8801de5d8518236a95cee7b4093196e55e11f6fb
Gil Lopes Teixeira <beerkiller@sapo.pt>
2021-04-15 12:09:01 +0100

    fixed ex2 type of foreign keys and created ex 4

--- p4/class_4_ex_2.sql
+++ p4/class_4_ex_2.sql
@@ -1,65 +1,65 @@
 create schema class_4_ex_2;
 
 create table class_4_ex_2.Airport(
-    ID primary key INT,
+    ID primary key identity(1,1) not null,
     city string not null,
     state string not null,
     name string not null
 );
 create table class_4_ex_2.Airplane(
-    ID primary key INT,
+    ID primary key identity(1,1) not null,
     type foreign key references class_4_ex_2.AirplaneType(tyoe_name) string not null,
     total_seats INT not null
 );
 
 create table class_4_ex_2.AirplaneType(
-    type_name primary key string,
+    type_name primary key string not null,
     company string not null,
     max_seats INT not null
 );
 
 create table class_4_ex_2.FlightLeg(
-    ID primary key INT,
-    flight_ID primary key foreign references class_4_ex_2.Flight(ID) key INT,
-    departure_airport_ID INT foreign key references class_4_ex_2.Airport(ID) not null,
-    arrival_airport_ID INT foreign key references class_4_ex_2.Airport(ID) not null,
+    ID primary key identity(1,1) not null,
+    flight_ID primary key foreign key references class_4_ex_2.Flight(ID) INT not null,
+    departure_airport_ID foreign key references class_4_ex_2.Airport(ID) not null,
+    arrival_airport_ID foreign key references class_4_ex_2.Airport(ID) INT not null,
     departure_time INT not null,
     arrival_time INT not null
 );
 
 create table class_4_ex_2.LegInstance(
-    flight_ID primary key foreign references class4_ex_2.FlightLeg(flight_ID) key INT,
-    leg_ID primary key foreign key references class4_ex_2.FlightLeg(ID) INT,
-    leg_date primary key DATE,
+    flight_ID primary key foreign key references class4_ex_2.FlightLeg(flight_ID) INT not null,
+    leg_ID primary key foreign key references class4_ex_2.FlightLeg(ID) INT not null,
+    leg_date primary key DATE not null,
     available_seats INT not null,
     airplane_ID foreign key INT not null,
-    departure_airport_ID INT foreign key references class4_ex_2.Airport(ID) not null,
-    arrival_airport_ID INT foreign key references class4_ex_2.Airport(ID) not null,
+    departure_airport_ID foreign key references class4_ex_2.Airport(ID) INT not null,
+    arrival_airport_ID foreign key references class4_ex_2.Airport(ID) INT not null,
     departure_time INT not null,
     arrival_time INT not null
 );
 
 create table class_4_ex_2.Seat(
-    seat_ID primary key INT,
-    flight_ID primary key foreign key references class4_ex_2.LegInstance(flight_ID) INT,
-    leg_ID primary key foreign key references class4_ex_2.LegInstance(ID) INT,
-    leg_date primary key foreign key references class4_ex_2.LegInstance(leg_date) DATE,
+    seat_ID primary key identity(1,1),
+    flight_ID primary key foreign key references class4_ex_2.LegInstance(flight_ID) INT not null,
+    leg_ID primary key foreign key references class4_ex_2.LegInstance(ID) INT not null,
+    leg_date primary key foreign key references class4_ex_2.LegInstance(leg_date) DATE not null,
 );
 
 create table class_4_ex_2.Fare(
-    ID primary key INT,
-    flight_ID primary key foreign key references class4_ex_2.Flight(ID) INT,
+    ID primary key identity(1,1) not null,
+    flight_ID primary key foreign key references class4_ex_2.Flight(ID) INT not null,
     amount INT not null,
     restrictions string not null,
 );
 
 create table class_4_ex_2.Flight(
-    ID primary key INT,
-    airline string not null,
-    weekdays string not null
+    ID primary key identity(1,1) not null,
+    airline string not null not null,
+    weekdays string not null not null
 );
 
 create table class_4_ex_2.CanLand(
-    airport_ID primary key foreign key references class4_ex_2.Airport(ID) INT,
-    airplane_ID primary key foreign key references class4_ex_2.Airport(ID) INT
+    airport_ID primary key foreign key references class4_ex_2.Airport(ID) INT not null,
+    airplane_ID primary key foreign key references class4_ex_2.Airport(ID) INT not null
 );
