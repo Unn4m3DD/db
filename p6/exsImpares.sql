b73290512490c5163bfa78bb8e991587418b38f8
Gil Lopes Teixeira <beerkiller@sapo.pt>
2021-05-08 14:57:21 +0100

    updated with the last exs

--- p6/exsImpares.sql
+++ p6/exsImpares.sql
@@ -33,10 +33,19 @@ group by [type]
 having avg(advance) is not null
 ) as temp on titles.type = temp.type and titles.advance >= temp.average * 1.5 
 
--- o) falta acabar
-select distinct title, avg(price * ytd_sales) as faturacao, avg(ytd_sales * royaltyper) as auths_revenue from titles
+-- o) 
+select distinct title, avg(price * ytd_sales) as faturacao, avg(royalty * 0.01 * price * ytd_sales) as auths_revenue, avg(price * ytd_sales) - avg(royalty * 0.01 * price * ytd_sales) as publisher_revenue from titles
 join titleauthor on titles.title_id = titleauthor.title_id
 where ytd_sales is not null
 group by titles.title_id, title 
 order by title
+
+-- q) 
+declare @title_count as int;
+set @title_count = (select count(*) from titles)
+select stores.stor_id, stor_name, count(*)  from stores
+join sales on stores.stor_id = sales.stor_id 
+group by stores.stor_id, stor_name
+having count(*) = @title_count
+--
 
