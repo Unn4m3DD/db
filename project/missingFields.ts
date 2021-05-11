70c715161ad6d27262fac4aec8bfdd9ceb198d4c
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-05-11 18:08:37 +0100

    minor bug fix

--- project/missingFields.ts
+++ project/missingFields.ts
@@ -1,22 +1,17 @@
-interface cpm_t {
-    station_id: number, //missing field
-}
-
 interface cam_t {
-    station_id: number, //missing field
-    last_power_status: number, //missing field
-    current_app_version: number//missing field
+  last_power_status: number, //missing field
+  current_app_version: number//missing field
 }
 
 interface vam_t {
-    station_id: number, //missing field
-    last_power_status: number, //missing field
-    configured_language: string, //missing field
-    current_app_version: number//missing field
+  last_power_status: number, //missing field
+  configured_language: string, //missing field
+  current_app_version: number//missing field
 }
 
 interface denm_t {
-    station_id: number, //missing field
-    configured_language: string, //missing field
-    current_app_version: number//missing field
+  device_type: "web" | "smarthphone",
+  configured_language?: string, //missing field
+  browser_version?: string, //missing field
+  current_app_version: number //missing field
 }
