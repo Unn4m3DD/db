016dc560480419c38f83658d9fb5797309778279
Gil Lopes Teixeira <beerkiller@sapo.pt>
2021-06-02 17:40:03 +0100

    fixed the ex c trigger to accept multi value inserts

--- P10/c.sql
+++ P10/c.sql
@@ -3,13 +3,27 @@ GO
 CREATE TRIGGER class_10.repeated_mgr on class_10.department
 INSTEAD OF INSERT, UPDATE
 AS
-    DECLARE @mgr_ssn as int
-    SELECT @mgr_ssn = Mgr_ssn from inserted;
-    IF EXISTS( SELECT * FROM class_10.department where Mgr_ssn = @mgr_ssn)
-        BEGIN
-            RAISERROR ('Já existe um departamento com esse manager', 16,1);
-            ROLLBACK TRAN; 
-        END
-	ELSE
-		INSERT INTO department SELECT * FROM inserted;
+	BEGIN TRANSACTION;
+    DECLARE @Dname VARCHAR(100), @Dnumber int, @Mgr_ssn int, @Mgr_start_date date;
+	DECLARE [cursor] CURSOR FOR
+      SELECT * FROM inserted
+    OPEN [cursor]
+    FETCH NEXT FROM [cursor] INTO @Dname, @Dnumber, @Mgr_ssn, @Mgr_start_date;
+    WHILE @@FETCH_STATUS = 0
+	BEGIN
+		PRINT @Mgr_ssn;
+		IF NOT EXISTS(SELECT * FROM class_10.department where department.Mgr_ssn = @Mgr_ssn)
+			BEGIN
+				PRINT 'insert';
+				INSERT INTO department values(@Dname, @Dnumber, @Mgr_ssn, @Mgr_start_date);
+			END
+		ELSE
+			BEGIN
+				RAISERROR ('Já existe um departamento com esse manager', 16,1);
+				--ROLLBACK TRANSACTION
+			END
+		FETCH NEXT FROM [cursor] INTO @Dname, @Dnumber, @Mgr_ssn, @Mgr_start_date;
+	END
+	CLOSE [cursor]
+    DEALLOCATE [cursor]
 GO
